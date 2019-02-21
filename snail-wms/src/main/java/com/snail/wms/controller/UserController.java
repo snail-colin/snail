edbff0450408dc158701ec027c2ae1b3de420471
snail-colin <609470156@qq.com>
2019-02-21 09:58:47 +0800

    测试自动化部署01

--- snail-wms/src/main/java/com/snail/wms/controller/UserController.java
+++ snail-wms/src/main/java/com/snail/wms/controller/UserController.java
@@ -84,6 +84,8 @@ public class UserController extends BaseController {
     @DeleteMapping("/user/{id}")
     public Result<String> delUser(@PathVariable Integer id) {
         System.out.println("测试一下");
+        System.out.println("测试一下");
+        System.out.println("测试一下");
         return sysUserService.delUser(id);
     }
 
